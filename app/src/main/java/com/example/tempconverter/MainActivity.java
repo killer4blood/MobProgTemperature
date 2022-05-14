5489d7090adf0abe7014b9f1d8f73adf02e78daf
Felicia <grandline.fl@gmail.com>
2022-05-14 22:08:32 +0700

    done with the celcius

--- app/src/main/java/com/example/tempconverter/MainActivity.java
+++ app/src/main/java/com/example/tempconverter/MainActivity.java
@@ -3,10 +3,23 @@ package com.example.tempconverter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.text.DecimalFormat;
 
@@ -24,7 +37,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         cityname = findViewById(R.id.ETextCityName);
-        //search = findViewById(R.id.btnSearch);
         celcius = findViewById(R.id.btnCelcius);
         fahrenheit = findViewById(R.id.btnFahrenheit);
         temperature = findViewById(R.id.TViewTemp);
@@ -32,5 +44,33 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void getWeatherDetails(View view) {
+        String tempURL = "";
+        String city = cityname.getText().toString().trim();
+        if (city.equals("")){
+            Toast.makeText(getApplicationContext(), "City name cannot be empty", Toast.LENGTH_SHORT).show();
+        } else {
+            tempURL = url + "?q=" + city + "&appid=" + appid;
+            StringRequest stringrequest = new StringRequest(Request.Method.POST, tempURL, new Response.Listener<String>() {
+                @Override
+                public void onResponse(String response) {
+                    try {
+                        JSONObject jsonresponse = new JSONObject(response);
+                        JSONObject jsonobjectmain = jsonresponse.getJSONObject("main");
+                        double temp = jsonobjectmain.getDouble("temp") - 273.15; //this is to convert to celsius
+                        temperature.setText(df.format(temp) + "°C");
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }, new Response.ErrorListener(){
+
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Toast.makeText(getApplicationContext(), error.toString().trim(), Toast.LENGTH_SHORT).show();
+                }
+            });
+            RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
+            requestQueue.add(stringrequest);
+        }
     }
 }
